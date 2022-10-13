ee181e4b23fc2a3fd6215c744a74fcdf6c51bb85
Romain Da Silva <romaindasilva@MacBook-Pro-de-Romain.local>
2022-10-13 10:34:11 +0200

    Docker update and Language CRUD

---
+++ src/Controller/LanguageController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Language;
+use App\Form\LanguageType;
+use App\Repository\LanguageRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/language')]
+class LanguageController extends AbstractController
+{
+    #[Route('/', name: 'app_language_index', methods: ['GET'])]
+    public function index(LanguageRepository $languageRepository): Response
+    {
+        return $this->render('language/index.html.twig', [
+            'languages' => $languageRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_language_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, LanguageRepository $languageRepository): Response
+    {
+        $language = new Language();
+        $form = $this->createForm(LanguageType::class, $language);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $languageRepository->save($language, true);
+
+            return $this->redirectToRoute('app_language_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('language/new.html.twig', [
+            'language' => $language,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_language_show', methods: ['GET'])]
+    public function show(Language $language): Response
+    {
+        return $this->render('language/show.html.twig', [
+            'language' => $language,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_language_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Language $language, LanguageRepository $languageRepository): Response
+    {
+        $form = $this->createForm(LanguageType::class, $language);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $languageRepository->save($language, true);
+
+            return $this->redirectToRoute('app_language_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('language/edit.html.twig', [
+            'language' => $language,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_language_delete', methods: ['POST'])]
+    public function delete(Request $request, Language $language, LanguageRepository $languageRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$language->getId(), $request->request->get('_token'))) {
+            $languageRepository->remove($language, true);
+        }
+
+        return $this->redirectToRoute('app_language_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/LanguageType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Language;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LanguageType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('typed')
+            ->add('description')
+            ->add('execution_speed')
+            ->add('developpement_speed')
+            ->add('documentation')
+            ->add('repository')
+            ->add('language_execution')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Language::class,
+        ]);
+    }
+}
