62dea7188eb98fb054b283604b925dba1950d455
romain.da-silva@etu.univ-smb.fr <romain.da-silva@etu.univ-smb.fr>
2022-10-14 15:46:16 +0200

    add auth

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Form\LoginFormType;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'app_login')]
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+
+        $form = $this->createForm(LoginFormType::class);
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('registration/login.html.twig', [
+            'loginForm' => $form->createView(),
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ]);
+    }
+}
